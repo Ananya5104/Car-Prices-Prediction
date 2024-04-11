9e98205fd79b50b955577dfd67e546debaab013e
Ananya5104 <saiananyakatakam@gmail.com>
2024-04-12 00:13:45 +0530

    ver

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import pickle
 import streamlit as st
-import numpy as np  
 import pandas as pd
 import warnings
-import sklearn
 
 # Ignore all UserWarnings
 warnings.filterwarnings('ignore', category=UserWarning)
